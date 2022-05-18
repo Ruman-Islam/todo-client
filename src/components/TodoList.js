bbbd03ee5a571cda30ee4a6283d329eb257e5c9c
Ruman-Islam <rumanislam48@gmail.com>
2022-05-19 05:36:59 +0600

    use query used

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../firebase/firebaseConfig';
 
 const TodoList = () => {
+    const [user, ,] = useAuthState(auth);
+    console.log(user.email);
+
+    const { data, isLoading } = useQuery(['get-todo', user?.email], () =>
+        fetch(`http://localhost:5000/get-task?email=${user?.email}`).then(res => {
+            console.log(res);
+            return res.json();
+        }))
+    console.log(data);
+
+    if (isLoading) {
+        return <h1 style={{ paddingTop: '300px' }} className='text-center'>Please wait....</h1>
+    }
+
     return (
         <div className='bg-light p-5 rounded mt-3'>
             <div className='border-bottom'>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </BrowserRouter>
 );
